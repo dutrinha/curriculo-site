21e1ec8f1ae6df74f26817d4ec7835569c4d23f2
dutrinha <106093590+dutrinha@users.noreply.github.com>
2024-03-12 23:48:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -120,3 +120,48 @@ bodyContent.appendChild(mainContent);
 document.open();
 document.write(bodyContent.innerHTML);
 document.close();
+
+
+
+
+
+function toggleText(triggerSelector, containerSelector) {
+    const trigger = document.querySelector(triggerSelector);
+    const container = document.querySelector(containerSelector);
+  
+    trigger.addEventListener('click', () => {
+      container.classList.toggle('visible');
+      
+    });
+  }
+  
+  // Call the function with the appropriate selectors
+  toggleText('#trigger-btn', '#text-container',);
+
+
+  function toggleText(triggerSelector, containerSelector) {
+    const trigger = document.querySelector(triggerSelector);
+    const container = document.querySelector(containerSelector);
+  
+    trigger.addEventListener('click', () => {
+      container.classList.toggle('visible');
+      
+    });
+  }
+  
+  // Call the function with the appropriate selectors
+  toggleText('#trigger-btn2', '#text-container2',);
+
+
+  function toggleText(triggerSelector, containerSelector) {
+    const trigger = document.querySelector(triggerSelector);
+    const container = document.querySelector(containerSelector);
+  
+    trigger.addEventListener('click', () => {
+      container.classList.toggle('visible');
+      
+    });
+  }
+  
+  // Call the function with the appropriate selectors
+  toggleText('#trigger-btn3', '#text-container3',); 
